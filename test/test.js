bdc8243621d494e7c4e19d343bf83c8a4292bb1e
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-05-04 20:56:25 -0400

    tests: remove connect dependency

--- test/test.js
+++ test/test.js
@@ -1,25 +1,53 @@
-var connect = require('connect');
+
+var http = require('http');
+var morgan = require('..');
 var should = require('should');
 var request = require('supertest');
-
-var logger = require('..');
 
 var lastLogLine;
 function saveLastLogLine(line) { lastLogLine = line; }
 
 describe('logger()', function () {
+  it('should be able to skip based on request', function (done) {
+    function skip(req) { return ~req.url.indexOf('skip=true'); }
+
+    var server = createServer({'format': 'default', 'skip': skip, 'stream': {'write': saveLastLogLine}});
+
+    lastLogLine = null;
+
+    request(server)
+    .get('/?skip=true')
+    .set('Connection', 'close')
+    .end(function (err, res) {
+      if (err) return done(err);
+      should.not.exist(lastLogLine);
+      done();
+    });
+  });
+
+  it('should be able to skip based on response', function (done) {
+    function skip(req, res) { return res.statusCode === 200; }
+
+    var server = createServer({'format': 'default', 'skip': skip, 'stream': {'write': saveLastLogLine}});
+
+    lastLogLine = null;
+
+    request(server)
+    .get('/')
+    .end(function (err, res) {
+      if (err) return done(err);
+      should.not.exist(lastLogLine);
+      done();
+    });
+  });
+
   describe('when Connection: close', function () {
     it('should log the client ip', function (done) {
-      var app = connect();
-
-      app.use(logger({'format': 'default', 'stream': {'write': saveLastLogLine}}));
-      app.use(function (req, res) {
-        res.end(req.connection.remoteAddress);
-      });
+      var server = createServer({'format': 'default', 'stream': {'write': saveLastLogLine}});
 
       lastLogLine = null;
 
-      request(app.listen())
+      request(server)
       .get('/')
       .set('Connection', 'close')
       .end(function (err, res) {
@@ -27,56 +55,18 @@ describe('logger()', function () {
         lastLogLine.should.startWith(res.text);
         done();
       });
-    })
-
-    it('should be able to skip based on request', function (done) {
-      var app = connect();
-
-      function skip(req) { return req.query.skip; }
-
-      app.use(function (req, res, next) {
-        req.query = {
-          skip: true
-        };
-        next();
-      })
-      app.use(logger({'format': 'default', 'skip': skip, 'stream': {'write': saveLastLogLine}}));
-      app.use(function (req, res) {
-        res.end(req.connection.remoteAddress);
-      });
-
-      lastLogLine = null;
-
-      request(app.listen())
-      .get('/?skip=true')
-      .set('Connection', 'close')
-      .end(function (err, res) {
-        if (err) return done(err);
-        should.not.exist(lastLogLine);
-        done();
-      });
     });
-
-    it('should be able to skip based on response', function (done) {
-      var app = connect();
-
-      function skip(req, res) { return res.statusCode === 304; }
-
-      app.use(logger({'format': 'default', 'skip': skip, 'stream': {'write': saveLastLogLine}}));
-      app.use(function (req, res) {
-        res.statusCode = 304;
-        res.end(req.connection.remoteAddress);
-      });
-
-      lastLogLine = null;
-
-      request(app.listen())
-      .get('/')
-      .end(function (err, res) {
-        if (err) return done(err);
-        should.not.exist(lastLogLine);
-        done();
-      });
-    });
-  })
+  });
 });
+
+function createServer(opts) {
+  var logger = morgan(opts);
+  var server = http.createServer(function onRequest(req, res) {
+    logger(req, res, function onNext(err) {
+      res.statusCode = err ? 500 : 200;
+      res.end(err ? err.stack : String(req.connection.remoteAddress));
+    });
+  });
+
+  return server;
+}
