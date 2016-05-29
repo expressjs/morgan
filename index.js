3f10a43d4d0dd5b044ec4ab679581b70f1ea18d0
Douglas Christopher Wilson <doug@somethingdoug.com>
2016-05-29 00:16:10 -0400

    perf: enable strict mode in compiled functions

--- index.js
+++ index.js
@@ -376,7 +376,7 @@ function compile(format) {
   }
 
   var fmt = format.replace(/"/g, '\\"')
-  var js = '  return "' + fmt.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/g, function (_, name, arg) {
+  var js = '  "use strict"\n  return "' + fmt.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/g, function (_, name, arg) {
     var tokenArguments = 'req, res'
     var tokenFunction = 'tokens[' + String(JSON.stringify(name)) + ']'
 
@@ -385,7 +385,7 @@ function compile(format) {
     }
 
     return '" +\n    (' + tokenFunction + '(' + tokenArguments + ') || "-") + "'
-  }) + '";'
+  }) + '"'
 
   return new Function('tokens, req, res', js)
 }
